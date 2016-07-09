2955eaff24db4f9dc29a191566f60454173146a9
arjun1321 <guptaarjun152@gmail.com>
2016-07-09 16:40:12 +0530

    created basic layout

---
+++ index.php
@@ -0,0 +1,46 @@
+<html>
+    <head>
+        <title>Search Filter</title>
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+        <link rel="stylesheet" href="style.css" type="text/css">
+    </head>
+    
+    <body>
+       
+        <div id="header">
+            <h1>Hireshort Internship Test</h1>
+        </div>
+        
+        <div id="main-content">
+           
+           <div id="search-box">
+               <h1>Search Filter</h1>
+               <p>Note: Enter both or either one of the following field</p>
+               
+               <form action="" method="post">
+               <label for="height">Height</label>
+               <div class="form-group">
+                   <input type="number" class="form-control" name="height">
+               </div> 
+               
+               <label for="location">location</label>
+               <div class="form-group">
+                   <input type="text" class="form-control" name="location">
+               </div>
+               
+               <input type="submit" name="submit" class="btn btn-primary" value="submit">
+               
+           </form>
+           </div>
+           <br><br>
+           <hr>
+           
+           
+            
+        </div>
+        
+        <div id="footer">
+            <p>&copy;- 2016 <a href="http://github.com/arjun1321">Arjun Kumar</a></p>
+        </div>
+    </body>
+</html>
